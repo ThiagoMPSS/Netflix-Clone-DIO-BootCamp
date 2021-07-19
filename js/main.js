011d73eca9271e55b8176e779b0883fbf9642358
Thiago MP <thiago_guar@hotmail.com>
2021-07-19 13:39:22 -0300

    First!

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    // navText: false,
+    responsive:{
+        0:{
+            items:2,
+            nav:true
+        },
+        600:{
+            items:4,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
